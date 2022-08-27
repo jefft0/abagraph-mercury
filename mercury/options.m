b92a67da7afb88265677127fac2c89f6b1f1701e
jefft0 <jeff@thefirst.org>
2022-08-27 12:56:36 +0200

    options: Set print_to_file default to true.

--- mercury/options.m
+++ mercury/options.m
@@ -50,7 +50,7 @@ option(fileID, "_sol_").
 option(num_sols, "0").    % all solutions
 option(opponent_abagraph_choice, "s").
 option(opponent_sentence_choice, "p").
-option(print_to_file, "fail").
+option(print_to_file, "true").
 option(proponent_sentence_choice, "p").
 option(proponent_rule_choice, "l1").
 option(show_solution, "true").
