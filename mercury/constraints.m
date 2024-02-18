805568d2d91ff6a131dac80beb88525213f36b7a
jefft0 <jeff@thefirst.org>
2024-02-18 09:33:16 +0100

    constraints: Change s to '='.

--- mercury/constraints.m
+++ mercury/constraints.m
@@ -6,7 +6,7 @@
 :- type variable(T) ---> var(int).
 
 :- type constraint
-   ---> s(string).
+   ---> '='(string).
 
 :- type constraints == map(int, constraint).
 
@@ -21,20 +21,19 @@
 
 init = map.init.
 
-% If there is no binding for V, add one for V and C and set Desc to a description string (only if verbose).
+% If there is no binding for V, add one for V and the constraint and set Desc to
+% a description string (only if verbose).
 % If there is a binding for V, confirm the constraint and set Desc to "",
 % else if the constraint is not confirmed then fail.
-unify(V, C, Bs, BsOut, Desc) :-
-  (search(Bs, V, BoundC) ->
-    % TODO: Does this fail properly?
-    C = BoundC,
+unify(V, '='(Val), Bs, BsOut, Desc) :-
+  (search(Bs, V, '='(BoundVal)) ->
+    Val = BoundVal,
     BsOut = Bs,
     Desc = ""
   ;
     % Add the binding.
-    BsOut = insert(Bs, V, C),
+    BsOut = insert(Bs, V, '='(Val)),
     (verbose ->
-      C = s(String),
-      Desc = format("(var %i) = %s", [i(V), s(String)])
+      Desc = format("(var %i) = %s", [i(V), s(Val)])
     ; 
       Desc = "")).
