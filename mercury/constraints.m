e9a48b08f1f33fd5953afd17911c96c3fb2a8efb
jefft0 <jeff@thefirst.org>
2024-02-18 14:37:16 +0100

    constraints: In unify, return set of Desc.

--- mercury/constraints.m
+++ mercury/constraints.m
@@ -2,6 +2,7 @@
 :- interface.
 
 :- import_module map.
+:- import_module set.
 
 :- type variable(T) ---> var(int).
 
@@ -18,7 +19,7 @@
 :- type constraints == map(int, constraint).
 
 :- func init = constraints.
-:- pred unify(int::in, constraint::in, constraints::in, constraints::out, string::out) is semidet.
+:- pred unify(int::in, constraint::in, constraints::in, constraints::out, set(string)::out) is semidet.
 
 :- implementation.
 
@@ -28,31 +29,31 @@
 
 init = map.init.
 
-% If there is no binding for V, add one for V and the constraint and set Desc to
-% a description string (only if verbose).
-% If there is a binding for V, confirm the constraint and set Desc to "",
+% If there is no binding for V, add one for V and the constraint and set Descs to
+% a set of description strings for the bindings (only if verbose).
+% If there is a binding for V, confirm the constraint and set Descs to "",
 % else if the constraint is not confirmed then fail.
-unify(V, f('='(Val)), Bs, BsOut, Desc) :-
+unify(V, f('='(Val)), Bs, BsOut, Descs) :-
   (search(Bs, V, f('='(BoundVal))) ->
     Val = BoundVal,
     BsOut = Bs,
-    Desc = ""
+    Descs = set.init
   ;
     % Add the binding.
     BsOut = insert(Bs, V, f('='(Val))),
     (verbose ->
-      Desc = format("(var %i) = %f", [i(V), f(Val)])
+      Descs = make_singleton_set(format("(var %i) = %f", [i(V), f(Val)]))
     ; 
-      Desc = "")).
-unify(V, s('='(Val)), Bs, BsOut, Desc) :-
+      Descs = set.init)).
+unify(V, s('='(Val)), Bs, BsOut, Descs) :-
   (search(Bs, V, s('='(BoundVal))) ->
     Val = BoundVal,
     BsOut = Bs,
-    Desc = ""
+    Descs = set.init
   ;
     % Add the binding.
     BsOut = insert(Bs, V, s('='(Val))),
     (verbose ->
-      Desc = format("(var %i) = %s", [i(V), s(Val)])
+      Descs = make_singleton_set(format("(var %i) = %s", [i(V), s(Val)]))
     ; 
-      Desc = "")).
+      Descs = set.init)).
