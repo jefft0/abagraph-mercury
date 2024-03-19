f35119eda1c47cac9a63c74442856e92a39b6f0b
jefft0 <jeff@thefirst.org>
2024-03-19 08:05:21 +0100

    options: Set proponent_sentence_choice to eager.

--- mercury/options.m
+++ mercury/options.m
@@ -51,7 +51,7 @@ option(num_sols, "0").    % all solutions
 option(opponent_abagraph_choice, "s").
 option(opponent_sentence_choice, "p").
 option(print_to_file, "fail").
-option(proponent_sentence_choice, "p").
+option(proponent_sentence_choice, "e").
 option(proponent_rule_choice, "l1").
 option(show_solution, "true").
 option(turn_choice, "p").  % Use "p" instead of "[p,o]" like the web page documentation.
