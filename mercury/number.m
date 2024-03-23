b94a37e19a73288405fff84dd2afebe8a7cbbcb5
jefft0 <jeff@thefirst.org>
2024-03-23 09:16:06 +0100

    number: Add < and >= .

--- mercury/number.m
+++ mercury/number.m
@@ -6,11 +6,13 @@
 % (This is pretty basic. It seems like there should be an existing module somewhere.)
 :- typeclass number(T) where [
   func T + T = T,
-  func - (T) = T,
+  func -(T) = T,
   func T - T = T,
   pred (T::in) == (T::in) is semidet,
-  pred (T::in) =< (T::in) is semidet,
   pred (T::in) > (T::in) is semidet,
+  pred (T::in) < (T::in) is semidet,
+  pred (T::in) >= (T::in) is semidet,
+  pred (T::in) =< (T::in) is semidet,
   func to_string(T) = string
 ].
 
@@ -26,15 +28,19 @@
 
 % TODO: Make this more generalized.
 :- func f_tolerance = float is det.
-f_tolerance = 0.00001.
+:- func f_neg_tolerance = float is det.
+f_tolerance =      0.00001.
+f_neg_tolerance = -0.00001.
 
 :- instance number(float) where [
     func((+)/2) is float.(+),
     func((-)/1) is float.(-),
     func((-)/2) is float.(-),
-    (X == Y :- float.(float.(X - Y) =< f_tolerance), float.(float.(X - Y) >= float.(-f_tolerance))),
-    (X =< Y :- float.(float.(X - Y) =< f_tolerance)),
+    (X == Y :- float.(float.(X - Y) =< f_tolerance), float.(float.(X - Y) >= f_neg_tolerance)),
     (X > Y :- float.(float.(X - Y) > f_tolerance)),
+    (X < Y :- float.(float.(X - Y) < f_neg_tolerance)),
+    (X >= Y :- float.(float.(X - Y) >= f_neg_tolerance)),
+    (X =< Y :- float.(float.(X - Y) =< f_tolerance)),
     to_string(X) = format("%f", [f(X)])
 ].
 :- instance number(int) where [
@@ -42,8 +48,10 @@ f_tolerance = 0.00001.
     func((-)/1) is int.(-),
     func((-)/2) is int.(-),
     (X == Y :- X = Y),
-    pred((=<)/2) is int.(=<),
     pred((>)/2) is int.(>),
+    pred((<)/2) is int.(<),
+    pred((>=)/2) is int.(>=),
+    pred((=<)/2) is int.(=<),
     to_string(X) = format("%i", [i(X)])
 ].
 
