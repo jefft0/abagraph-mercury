052956a9dbbab27fc3770054e98d1ea4867f91d9
jefft0 <jeff@thefirst.org>
2024-05-25 16:11:24 +0200

    Add constraints_test

---
+++ mercury/test/constraints_test.m
@@ -0,0 +1,40 @@
+:- module constraints_test.
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module constraints.
+:- import_module list.
+:- import_module maybe.
+:- import_module string.
+
+:- pred test1 is semidet.
+% puts(S). Write the string S to stdout without a newline.
+:- pred puts(string::in) is det.
+% format(S, PolyTypes). Write string.format(S, PolyTypes) to stdout.
+:- pred format(string::in, list(poly_type)::in) is det.
+
+main(!IO) :-
+  (test1 -> 
+    format("Pass\n", [])
+  ;
+    format("Fail\n", [])).
+
+test1 :-
+  Cs = constraints.init,
+  unify(1, i('+'(var(2), 5)), Cs, Cs1, _),
+  unify(1, i(':='(20)), Cs1, Cs2, _),
+  i_get(2, Cs2) = yes(15).
+
+:- pragma foreign_proc("C",
+puts(S::in),
+[promise_pure],
+"
+fputs(S, stdout);
+").
+
+format(S, PolyTypes) :- puts(format(S, PolyTypes)).
