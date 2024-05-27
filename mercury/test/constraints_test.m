f6dddf256cd652400db2ce38e13b464ed153d430
jefft0 <jeff@thefirst.org>
2024-05-27 17:31:11 +0200

    constraints_test: Use a list of test predicates.

--- mercury/test/constraints_test.m
+++ mercury/test/constraints_test.m
@@ -8,90 +8,84 @@
 :- implementation.
 
 :- import_module constraints.
+:- import_module int.
 :- import_module list.
 :- import_module maybe.
 :- import_module string.
 
-:- pred test1 is semidet.
-:- pred test2 is semidet.
-:- pred test3 is semidet.
-:- pred test4 is semidet.
-:- pred test5 is semidet.
-:- pred test6 is semidet.
-:- pred test7 is semidet.
-:- pred test8 is semidet.
-:- pred test9 is semidet.
-:- pred test10 is semidet.
-:- pred test11 is semidet.
+:- pred run_test(pred(constraints)).
+:- mode run_test(pred(in) is semidet).
+
 % puts(S). Write the string S to stdout without a newline.
 :- pred puts(string::in) is det.
 % format(S, PolyTypes). Write string.format(S, PolyTypes) to stdout.
 :- pred format(string::in, list(poly_type)::in) is det.
 
 main(!IO) :-
-  (test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11 -> 
-    format("Pass\n", [])
-  ;
-    format("Fail\n", [])).
+  Tests = [
+    % Test int +
+    (pred(CS::in) is semidet :-
+      unify(1, i('+'(var(2), 5)), CS, CS1, _),
+      unify(1, i(':='(20)), CS1, CS2, _),
+      i_get(2, CS2) = yes(15)),
 
-% Test int +
-test1 :-
-  Cs = constraints.init,
-  unify(1, i('+'(var(2), 5)), Cs, Cs1, _),
-  unify(1, i(':='(20)), Cs1, Cs2, _),
-  i_get(2, Cs2) = yes(15).
+    % Test string \=
+    (pred(CS::in) is semidet :-
+      unify(1, s('\\='("a")), CS, CS1, _),
+      unify(1, s(':='("b")), CS1, _, _)),
+    (pred(CS::in) is semidet :-
+      unify(1, s(':='("a")), CS, CS1, _),
+      unify(1, s('\\='("b")), CS1, _, _)),
+    (pred(CS::in) is semidet :-
+      unify(1, s('\\='("a")), CS, CS1, _),
+      \+ unify(1, s(':='("a")), CS1, _, _)),
+    (pred(CS::in) is semidet :-
+      unify(1, s(':='("a")), CS, CS1, _),
+      \+ unify(1, s('\\='("a")), CS1, _, _)),
 
-% Test string \=
-test2 :-
-  Cs = constraints.init,
-  unify(1, s('\\='("a")), Cs, Cs1, _),
-  unify(1, s(':='("b")), Cs1, _, _).
-test3 :-
-  Cs = constraints.init,
-  unify(1, s(':='("a")), Cs, Cs1, _),
-  unify(1, s('\\='("b")), Cs1, _, _).
-test4 :-
-  Cs = constraints.init,
-  unify(1, s('\\='("a")), Cs, Cs1, _),
-  \+ unify(1, s(':='("a")), Cs1, _, _).
-test5 :-
-  Cs = constraints.init,
-  unify(1, s(':='("a")), Cs, Cs1, _),
-  \+ unify(1, s('\\='("a")), Cs1, _, _).
+    % Test string \==
+    (pred(CS::in) is semidet :-
+      unify(1, s('\\=='(var(2))), CS, CS1, _),
+      unify(1, s(':='("a")), CS1, CS2, _),
+      unify(2, s(':='("b")), CS2, _, _)),
+    (pred(CS::in) is semidet :-
+      unify(1, s(':='("a")), CS, CS1, _),
+      unify(1, s('\\=='(var(2))), CS1, CS2, _),
+      unify(2, s(':='("b")), CS2, _, _)),
+    (pred(CS::in) is semidet :-
+      unify(1, s(':='("a")), CS, CS1, _),
+      unify(2, s(':='("b")), CS1, CS2, _),
+      unify(1, s('\\=='(var(2))), CS2, _, _)),
+    (pred(CS::in) is semidet :-
+      unify(1, s('\\=='(var(2))), CS, CS1, _),
+      unify(1, s(':='("a")), CS1, CS2, _),
+      \+ unify(2, s(':='("a")), CS2, _, _)),
+    (pred(CS::in) is semidet :-
+      unify(1, s(':='("a")), CS, CS1, _),
+      format("CS1\n%s", [s(to_string(CS1))]),
+      unify(1, s('\\=='(var(2))), CS1, CS2, _),
+      format("CS2\n%s", [s(to_string(CS2))]),
+      \+ unify(2, s(':='("a")), CS2, _, _)),
+    (pred(CS::in) is semidet :-
+      unify(1, s(':='("a")), CS, CS1, _),
+      unify(2, s(':='("a")), CS1, CS2, _),
+      \+ unify(1, s('\\=='(var(2))), CS2, _, _))
+  ],
 
-% Test string \==
-test6 :-
-  Cs = constraints.init,
-  unify(1, s('\\=='(var(2))), Cs, Cs1, _),
-  unify(1, s(':='("a")), Cs1, Cs2, _),
-  unify(2, s(':='("b")), Cs2, _, _).
-test7 :-
-  Cs = constraints.init,
-  unify(1, s(':='("a")), Cs, Cs1, _),
-  unify(1, s('\\=='(var(2))), Cs1, Cs2, _),
-  unify(2, s(':='("b")), Cs2, _, _).
-test8 :-
-  Cs = constraints.init,
-  unify(1, s(':='("a")), Cs, Cs1, _),
-  unify(2, s(':='("b")), Cs1, Cs2, _),
-  unify(1, s('\\=='(var(2))), Cs2, _, _).
-test9 :-
-  Cs = constraints.init,
-  unify(1, s('\\=='(var(2))), Cs, Cs1, _),
-  unify(1, s(':='("a")), Cs1, Cs2, _),
-  \+ unify(2, s(':='("a")), Cs2, _, _).
-test10 :-
-  Cs = constraints.init,
-  unify(1, s(':='("a")), Cs, Cs1, _),
-  format("Cs1\n%s", [s(to_string(Cs1))]),
-  unify(1, s('\\=='(var(2))), Cs1, Cs2, _),
-  format("Cs2\n%s", [s(to_string(Cs2))]),
-  \+ unify(2, s(':='("a")), Cs2, _, _).
-test11 :-
-  Cs = constraints.init,
-  unify(1, s(':='("a")), Cs, Cs1, _),
-  unify(2, s(':='("a")), Cs1, Cs2, _),
-  \+ unify(1, s('\\=='(var(2))), Cs2, _, _).
+  Tests = [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11],
+  run_test(T1),
+  run_test(T2),
+  run_test(T3),
+  run_test(T4),
+  run_test(T5),
+  run_test(T6),
+  run_test(T7),
+  run_test(T8),
+  run_test(T9),
+  run_test(T10),
+  run_test(T11).
+
+run_test(Test) :- (Test(constraints.init) -> format("Pass\n", []) ; format("Fail\n", [])).
 
 :- pragma foreign_proc("C",
 puts(S::in),
