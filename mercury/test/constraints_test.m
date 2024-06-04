f5ddb5d5c240e0cf4bcaff5064a9a7e84f2bf27e
jefft0 <jeff@thefirst.org>
2024-06-04 16:10:23 +0200

    constraints_test: Call run_test each time.

--- mercury/test/constraints_test.m
+++ mercury/test/constraints_test.m
@@ -22,229 +22,185 @@
 :- pred format(string::in, list(poly_type)::in) is det.
 
 main(!IO) :-
-  Tests = [
-    % Test int \=
-    (pred(CS::in) is semidet :-
-      unify(1, i('\\='(10)), CS, CS1, _),
-      unify(1, i(':='(11)), CS1, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(10)), CS, CS1, _),
-      unify(1, i('\\='(11)), CS1, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i('\\='(10)), CS, CS1, _),
-      \+ unify(1, i(':='(10)), CS1, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(10)), CS, CS1, _),
-      \+ unify(1, i('\\='(10)), CS1, _, _)),
+  % Test int \=
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i('\\='(10)), CS, CS1, _),
+    unify(1, i(':='(11)), CS1, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(10)), CS, CS1, _),
+    unify(1, i('\\='(11)), CS1, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i('\\='(10)), CS, CS1, _),
+    \+ unify(1, i(':='(10)), CS1, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(10)), CS, CS1, _),
+    \+ unify(1, i('\\='(10)), CS1, _, _))),
 
-    % Test int \==
-    (pred(CS::in) is semidet :-
-      unify(1, i('\\=='(var(2))), CS, CS1, _),
-      unify(1, i(':='(10)), CS1, CS2, _),
-      unify(2, i(':='(11)), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(10)), CS, CS1, _),
-      unify(1, i('\\=='(var(2))), CS1, CS2, _),
-      unify(2, i(':='(11)), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(2, i(':='(11)), CS, CS1, _),
-      unify(1, i('\\=='(var(2))), CS1, CS2, _),
-      unify(1, i(':='(10)), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(10)), CS, CS1, _),
-      unify(2, i(':='(11)), CS1, CS2, _),
-      unify(1, i('\\=='(var(2))), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i('\\=='(var(2))), CS, CS1, _),
-      unify(1, i(':='(10)), CS1, CS2, _),
-      \+ unify(2, i(':='(10)), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(10)), CS, CS1, _),
-      unify(1, i('\\=='(var(2))), CS1, CS2, _),
-      \+ unify(2, i(':='(10)), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(10)), CS, CS1, _),
-      unify(2, i(':='(10)), CS1, CS2, _),
-      \+ unify(1, i('\\=='(var(2))), CS2, _, _)),
+  % Test int \==
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i('\\=='(var(2))), CS, CS1, _),
+    unify(1, i(':='(10)), CS1, CS2, _),
+    unify(2, i(':='(11)), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(10)), CS, CS1, _),
+    unify(1, i('\\=='(var(2))), CS1, CS2, _),
+    unify(2, i(':='(11)), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(2, i(':='(11)), CS, CS1, _),
+    unify(1, i('\\=='(var(2))), CS1, CS2, _),
+    unify(1, i(':='(10)), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(10)), CS, CS1, _),
+    unify(2, i(':='(11)), CS1, CS2, _),
+    unify(1, i('\\=='(var(2))), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i('\\=='(var(2))), CS, CS1, _),
+    unify(1, i(':='(10)), CS1, CS2, _),
+    \+ unify(2, i(':='(10)), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(10)), CS, CS1, _),
+    unify(1, i('\\=='(var(2))), CS1, CS2, _),
+    \+ unify(2, i(':='(10)), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(10)), CS, CS1, _),
+    unify(2, i(':='(10)), CS1, CS2, _),
+    \+ unify(1, i('\\=='(var(2))), CS2, _, _))),
 
-    % Test int +
-    (pred(CS::in) is semidet :-
-      unify(1, i('+'(var(2), 5)), CS, CS1, _),
-      unify(1, i(':='(20)), CS1, CS2, _),
-      unify(2, i(':='(15)), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(20)), CS, CS1, _),
-      unify(1, i('+'(var(2), 5)), CS1, CS2, _),
-      unify(2, i(':='(15)), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(20)), CS, CS1, _),
-      unify(2, i(':='(15)), CS1, CS2, _),
-      unify(1, i('+'(var(2), 5)), CS2, _, _)),
+  % Test int +
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i('+'(var(2), 5)), CS, CS1, _),
+    unify(1, i(':='(20)), CS1, CS2, _),
+    unify(2, i(':='(15)), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(20)), CS, CS1, _),
+    unify(1, i('+'(var(2), 5)), CS1, CS2, _),
+    unify(2, i(':='(15)), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(20)), CS, CS1, _),
+    unify(2, i(':='(15)), CS1, CS2, _),
+    unify(1, i('+'(var(2), 5)), CS2, _, _))),
 
-    % Test int ++
-    (pred(CS::in) is semidet :-
-      unify(1, i('++'(var(2), var(3))), CS, CS1, _),
-      unify(1, i(':='(20)), CS1, CS2, _),
-      unify(2, i(':='(5)), CS2, CS3, _),
-      unify(3, i(':='(15)), CS3, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(20)), CS, CS1, _),
-      unify(1, i('++'(var(2), var(3))), CS1, CS2, _),
-      unify(2, i(':='(5)), CS2, CS3, _),
-      unify(3, i(':='(15)), CS3, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(2, i(':='(5)), CS, CS1, _),
-      unify(1, i('++'(var(2), var(3))), CS1, CS2, _),
-      unify(1, i(':='(20)), CS2, CS3, _),
-      unify(3, i(':='(15)), CS3, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(20)), CS, CS1, _),
-      unify(2, i(':='(5)), CS1, CS2, _),
-      unify(1, i('++'(var(2), var(3))), CS2, CS3, _),
-      unify(3, i(':='(15)), CS3, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(20)), CS, CS1, _),
-      unify(2, i(':='(5)), CS1, CS2, _),
-      unify(3, i(':='(15)), CS2, CS3, _),
-      unify(1, i('++'(var(2), var(3))), CS3, _, _)),
+  % Test int ++
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i('++'(var(2), var(3))), CS, CS1, _),
+    unify(1, i(':='(20)), CS1, CS2, _),
+    unify(2, i(':='(5)), CS2, CS3, _),
+    unify(3, i(':='(15)), CS3, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(20)), CS, CS1, _),
+    unify(1, i('++'(var(2), var(3))), CS1, CS2, _),
+    unify(2, i(':='(5)), CS2, CS3, _),
+    unify(3, i(':='(15)), CS3, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(2, i(':='(5)), CS, CS1, _),
+    unify(1, i('++'(var(2), var(3))), CS1, CS2, _),
+    unify(1, i(':='(20)), CS2, CS3, _),
+    unify(3, i(':='(15)), CS3, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(20)), CS, CS1, _),
+    unify(2, i(':='(5)), CS1, CS2, _),
+    unify(1, i('++'(var(2), var(3))), CS2, CS3, _),
+    unify(3, i(':='(15)), CS3, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(20)), CS, CS1, _),
+    unify(2, i(':='(5)), CS1, CS2, _),
+    unify(3, i(':='(15)), CS2, CS3, _),
+    unify(1, i('++'(var(2), var(3))), CS3, _, _))),
 
-    % Test int -
-    (pred(CS::in) is semidet :-
-      unify(1, i('-'(20, var(2))), CS, CS1, _),
-      unify(1, i(':='(15)), CS1, CS2, _),
-      unify(2, i(':='(5)), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(15)), CS, CS1, _),
-      unify(1, i('-'(20, var(2))), CS1, CS2, _),
-      unify(2, i(':='(5)), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(15)), CS, CS1, _),
-      unify(2, i(':='(5)), CS1, CS2, _),
-      unify(1, i('-'(20, var(2))), CS2, _, _)),
+  % Test int -
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i('-'(20, var(2))), CS, CS1, _),
+    unify(1, i(':='(15)), CS1, CS2, _),
+    unify(2, i(':='(5)), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(15)), CS, CS1, _),
+    unify(1, i('-'(20, var(2))), CS1, CS2, _),
+    unify(2, i(':='(5)), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(15)), CS, CS1, _),
+    unify(2, i(':='(5)), CS1, CS2, _),
+    unify(1, i('-'(20, var(2))), CS2, _, _))),
 
-    % Test int --
-    (pred(CS::in) is semidet :-
-      unify(1, i('--'(var(2), var(3))), CS, CS1, _),
-      unify(1, i(':='(15)), CS1, CS2, _),
-      unify(2, i(':='(20)), CS2, CS3, _),
-      unify(3, i(':='(5)), CS3, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(15)), CS, CS1, _),
-      unify(1, i('--'(var(2), var(3))), CS1, CS2, _),
-      unify(2, i(':='(20)), CS2, CS3, _),
-      unify(3, i(':='(5)), CS3, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(3, i(':='(5)), CS, CS1, _),
-      unify(1, i('--'(var(2), var(3))), CS1, CS2, _),
-      unify(1, i(':='(15)), CS2, CS3, _),
-      unify(2, i(':='(20)), CS3, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(2, i(':='(20)), CS, CS1, _),
-      format("CS1\n%s", [s(to_string(CS1))]),
-      unify(1, i('--'(var(2), var(3))), CS1, CS2, _),
-      format("CS2\n%s", [s(to_string(CS2))]),
-      unify(1, i(':='(15)), CS2, CS3, _),
-      format("CS3\n%s", [s(to_string(CS3))]),
-      unify(3, i(':='(5)), CS3, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(15)), CS, CS1, _),
-      unify(2, i(':='(20)), CS1, CS2, _),
-      unify(1, i('--'(var(2), var(3))), CS2, CS3, _),
-      unify(3, i(':='(5)), CS3, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, i(':='(15)), CS, CS1, _),
-      unify(2, i(':='(20)), CS1, CS2, _),
-      unify(3, i(':='(5)), CS2, CS3, _),
-      unify(1, i('--'(var(2), var(3))), CS3, _, _)),
+  % Test int --
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i('--'(var(2), var(3))), CS, CS1, _),
+    unify(1, i(':='(15)), CS1, CS2, _),
+    unify(2, i(':='(20)), CS2, CS3, _),
+    unify(3, i(':='(5)), CS3, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(15)), CS, CS1, _),
+    unify(1, i('--'(var(2), var(3))), CS1, CS2, _),
+    unify(2, i(':='(20)), CS2, CS3, _),
+    unify(3, i(':='(5)), CS3, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(3, i(':='(5)), CS, CS1, _),
+    unify(1, i('--'(var(2), var(3))), CS1, CS2, _),
+    unify(1, i(':='(15)), CS2, CS3, _),
+    unify(2, i(':='(20)), CS3, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(2, i(':='(20)), CS, CS1, _),
+    format("CS1\n%s", [s(to_string(CS1))]),
+    unify(1, i('--'(var(2), var(3))), CS1, CS2, _),
+    format("CS2\n%s", [s(to_string(CS2))]),
+    unify(1, i(':='(15)), CS2, CS3, _),
+    format("CS3\n%s", [s(to_string(CS3))]),
+    unify(3, i(':='(5)), CS3, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(15)), CS, CS1, _),
+    unify(2, i(':='(20)), CS1, CS2, _),
+    unify(1, i('--'(var(2), var(3))), CS2, CS3, _),
+    unify(3, i(':='(5)), CS3, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, i(':='(15)), CS, CS1, _),
+    unify(2, i(':='(20)), CS1, CS2, _),
+    unify(3, i(':='(5)), CS2, CS3, _),
+    unify(1, i('--'(var(2), var(3))), CS3, _, _))),
 
-    % Test string \=
-    (pred(CS::in) is semidet :-
-      unify(1, s('\\='("a")), CS, CS1, _),
-      unify(1, s(':='("b")), CS1, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, s(':='("a")), CS, CS1, _),
-      unify(1, s('\\='("b")), CS1, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, s('\\='("a")), CS, CS1, _),
-      \+ unify(1, s(':='("a")), CS1, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, s(':='("a")), CS, CS1, _),
-      \+ unify(1, s('\\='("a")), CS1, _, _)),
+  % Test string \=
+  run_test((pred(CS::in) is semidet :-
+    unify(1, s('\\='("a")), CS, CS1, _),
+    unify(1, s(':='("b")), CS1, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, s(':='("a")), CS, CS1, _),
+    unify(1, s('\\='("b")), CS1, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, s('\\='("a")), CS, CS1, _),
+    \+ unify(1, s(':='("a")), CS1, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, s(':='("a")), CS, CS1, _),
+    \+ unify(1, s('\\='("a")), CS1, _, _))),
 
-    % Test string \==
-    (pred(CS::in) is semidet :-
-      unify(1, s('\\=='(var(2))), CS, CS1, _),
-      unify(1, s(':='("a")), CS1, CS2, _),
-      unify(2, s(':='("b")), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, s(':='("a")), CS, CS1, _),
-      unify(1, s('\\=='(var(2))), CS1, CS2, _),
-      unify(2, s(':='("b")), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(2, s(':='("b")), CS, CS1, _),
-      unify(1, s('\\=='(var(2))), CS1, CS2, _),
-      unify(1, s(':='("a")), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, s(':='("a")), CS, CS1, _),
-      unify(2, s(':='("b")), CS1, CS2, _),
-      unify(1, s('\\=='(var(2))), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, s('\\=='(var(2))), CS, CS1, _),
-      unify(1, s(':='("a")), CS1, CS2, _),
-      \+ unify(2, s(':='("a")), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, s(':='("a")), CS, CS1, _),
-      unify(1, s('\\=='(var(2))), CS1, CS2, _),
-      \+ unify(2, s(':='("a")), CS2, _, _)),
-    (pred(CS::in) is semidet :-
-      unify(1, s(':='("a")), CS, CS1, _),
-      unify(2, s(':='("a")), CS1, CS2, _),
-      \+ unify(1, s('\\=='(var(2))), CS2, _, _))
-  ],
+  % Test string \==
+  run_test((pred(CS::in) is semidet :-
+    unify(1, s('\\=='(var(2))), CS, CS1, _),
+    unify(1, s(':='("a")), CS1, CS2, _),
+    unify(2, s(':='("b")), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, s(':='("a")), CS, CS1, _),
+    unify(1, s('\\=='(var(2))), CS1, CS2, _),
+    unify(2, s(':='("b")), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(2, s(':='("b")), CS, CS1, _),
+    unify(1, s('\\=='(var(2))), CS1, CS2, _),
+    unify(1, s(':='("a")), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, s(':='("a")), CS, CS1, _),
+    unify(2, s(':='("b")), CS1, CS2, _),
+    unify(1, s('\\=='(var(2))), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, s('\\=='(var(2))), CS, CS1, _),
+    unify(1, s(':='("a")), CS1, CS2, _),
+    \+ unify(2, s(':='("a")), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, s(':='("a")), CS, CS1, _),
+    unify(1, s('\\=='(var(2))), CS1, CS2, _),
+    \+ unify(2, s(':='("a")), CS2, _, _))),
+  run_test((pred(CS::in) is semidet :-
+    unify(1, s(':='("a")), CS, CS1, _),
+    unify(2, s(':='("a")), CS1, CS2, _),
+    \+ unify(1, s('\\=='(var(2))), CS2, _, _))).
 
-  Tests = [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29, T30,
-           T31, T32, T33, T34, T35, T36, T37, T38, T39],
-  run_test(T1),
-  run_test(T2),
-  run_test(T3),
-  run_test(T4),
-  run_test(T5),
-  run_test(T6),
-  run_test(T7),
-  run_test(T8),
-  run_test(T9),
-  run_test(T10),
-  run_test(T11),
-  run_test(T12),
-  run_test(T13),
-  run_test(T14),
-  run_test(T15),
-  run_test(T16),
-  run_test(T17),
-  run_test(T18),
-  run_test(T19),
-  run_test(T20),
-  run_test(T21),
-  run_test(T22),
-  run_test(T23),
-  run_test(T24),
-  run_test(T25),
-  run_test(T26),
-  run_test(T27),
-  run_test(T28),
-  run_test(T29),
-  run_test(T30),
-  run_test(T31),
-  run_test(T32),
-  run_test(T33),
-  run_test(T34),
-  run_test(T35),
-  run_test(T36),
-  run_test(T37),
-  run_test(T38),
-  run_test(T39).
-
-run_test(Test) :- (Test(constraints.init) -> format("Pass\n", []) ; format("Fail\n", [])).
+run_test(Test) :- (Test(constraints.init) -> format("pass\n", []) ; format("FAIL\n", [])).
 
 :- pragma foreign_proc("C",
 puts(S::in),
